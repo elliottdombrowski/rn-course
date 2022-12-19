7b815471b77fdac34de2a3da0a2ada0636ad8d5c
elliott dombrowski <elliottroyal@me.com>
2022-12-18 23:18:03 -0600

    clean out

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "white",
-    fontSize: 24,
-  }
+
 });
