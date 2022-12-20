1b7ad0c65f04f084d7ff04d553080121c714d52d
elliott dombrowski <elliottroyal@me.com>
2022-12-20 00:03:45 -0600

    refactor to flatlist

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   View, 
   Button,
   ScrollView,
+  FlatList,
 } from 'react-native';
 
 export default function App() {
@@ -20,7 +21,7 @@ export default function App() {
   const addGoalHandler = () => {
     setCourseGoals(currentCourseGoals => [
       ...currentCourseGoals,
-      enteredGoalText
+      { text: enteredGoalText, id: Math.random().toString() }
     ]);
   };
 
@@ -39,18 +40,22 @@ export default function App() {
       </View>
 
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) =>
-            <View 
-              key={goal}
-              style={styles.goalItem}
-            >
-              <Text style={styles.goalText}>
-                {goal}
-              </Text>
-            </View>
-          )}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={itemData => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>
+                  {itemData.item.text}
+                </Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
