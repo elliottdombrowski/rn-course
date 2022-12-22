ac7c291fa67cbe6f06fd9ea6cbbd39d9c167213e
elliott dombrowski <elliottroyal@me.com>
2022-12-22 00:13:50 -0600

    componentized goal item

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import {
   StyleSheet,
-  Text,
   TextInput,
   View,
   Button,
-  ScrollView,
   FlatList,
 } from 'react-native';
+
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -43,13 +43,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={itemData => {
-            return (
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>
-                  {itemData.item.text}
-                </Text>
-              </View>
-            );
+            return <GoalItem />
           }}
           keyExtractor={(item, index) => {
             return item.id;
@@ -85,14 +79,5 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
   },
 });
