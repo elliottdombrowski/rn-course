eac61c2c37964c55ee54506ae89652776513c579
elliott dombrowski <elliottroyal@me.com>
2022-12-24 06:41:36 -0600

    android ripple

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,13 +2,13 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 const GoalItem = (props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable android_ripple={{ color: '#dddddd' }} onPress={props.onDeleteItem.bind(this, props.id)}>
         <Text style={styles.goalText}>
           {props.text}
         </Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -17,11 +17,11 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
+    padding: 8,
     color: 'white',
   },
 });
