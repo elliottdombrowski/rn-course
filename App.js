f6fda2670b6a9f7435460491f5566a48487a9a09
elliott dombrowski <elliottroyal@me.com>
2022-12-24 10:52:37 -0600

    status bar

--- App.js
+++ App.js
@@ -37,36 +37,39 @@ export default function App() {
   };
 
   return (
-    <View style={styles.appContainer}>
-      <Button 
-        title='Add New Goal' 
-        color='#5e0acc' 
-        onPress={startAddGoalHandler}
-      />
-
-      <GoalInput 
-        onAddGoal={addGoalHandler} 
-        visible={modalIsVisible} 
-        onCancel={endAddGoalHandler} 
-      />
-      
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={itemData => {
-            return <GoalItem
-              text={itemData.item.text}
-              id={itemData.item.id}
-              onDeleteItem={deleteGoalHandler}
-            />
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
+    <>
+      <StatusBar style='light' />
+      <View style={styles.appContainer}>
+        <Button
+          title='Add New Goal'
+          color='#a065ec'
+          onPress={startAddGoalHandler}
         />
+
+        <GoalInput
+          onAddGoal={addGoalHandler}
+          visible={modalIsVisible}
+          onCancel={endAddGoalHandler}
+        />
+
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={itemData => {
+              return <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
